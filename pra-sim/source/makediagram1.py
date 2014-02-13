c253ef5c7ed4b36911b81ec8dbd96cdeb96797cf
david78k <root@c11node6>
2014-02-13 16:26:31 -0500

    pr

--- pra-sim/source/makediagram1.py
+++ pra-sim/source/makediagram1.py
@@ -16,7 +16,7 @@ for i in [algorithms.FIFO, algorithms.SecondChance, algorithms.NRU,
           algorithms.LRU, algorithms.Aging]:
     # List of tupels of (ws_length, page_faults_to_accesses_ratio).
     ratios = []
-    for j in range(1, 61):
+    for j in range(1, 11): #, 61):
         mms = i(64)    # Instantiate.
         mms.shift = 200
         mms.firstbit = 1 << 7
@@ -27,7 +27,8 @@ for i in [algorithms.FIFO, algorithms.SecondChance, algorithms.NRU,
     d.append(Gnuplot.Data(ratios, title=i.__name__, inline=1))
 
 g = Gnuplot.Gnuplot()
-g('set data style lines')
+#g('set data style lines')
+g('set style data lines')
 g('set yrange [0:]')
 g('set terminal epslatex monochrome')
 g('set output "diagram1.eps"')
@@ -35,4 +36,8 @@ g('set xlabel "working set size in pages"')
 g('set ylabel "ratio of page faults to accesses"')
 g.plot(*d)
 
-print "\nNow move diagram1.* to ../"
+g('set terminal png')
+g('set output "diagram1.png"')
+g.plot(*d)
+
+#print "\nNow move diagram1.* to ../"
