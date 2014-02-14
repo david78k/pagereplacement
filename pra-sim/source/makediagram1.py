03c70cd6088eafc5d3f7e07c4b740d0b5619d47f
david78k <root@c11node6>
2014-02-13 20:06:18 -0500

    pr

--- pra-sim/source/makediagram1.py
+++ pra-sim/source/makediagram1.py
@@ -16,8 +16,8 @@ for i in [algorithms.FIFO, algorithms.SecondChance, algorithms.NRU,
           algorithms.LRU, algorithms.Aging]: #, algorithms.Optimal]:
     # List of tupels of (ws_length, page_faults_to_accesses_ratio).
     ratios = []
-    for j in range(1, 61):
     #for j in range(1, 5):
+    for j in range(1, 61):
         mms = i(64)    # Instantiate.
         mms.shift = 200
         mms.firstbit = 1 << 7
