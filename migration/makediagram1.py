459b5c3971b627efff5ab6e03f0148c653220e79
david78k <root@c11node6>
2014-02-18 13:59:02 -0500

    pr

---
+++ migration/makediagram1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""Make a diagram."""
+
+# Written by Felix Wiemann <http://www.ososo.de/>, licensed under GPL-2.
+# Edited by Tae Seung Kang, 2/18/2014
+
+import algorithms
+import access
+import random
+import simulate
+import Gnuplot
+
+
+d = []    # List of gnuplot data
+for i in [algorithms.FIFO, algorithms.SecondChance, algorithms.NRU,
+          algorithms.LRU, algorithms.Aging]: #, algorithms.Optimal]:
+    # List of tupels of (ws_length, page_faults_to_accesses_ratio).
+    ratios = []
+    for j in range(1, 5):
+    #for j in range(1, 61):
+        mms = i(64)    # Instantiate.
+        mms.shift = 200
+        mms.firstbit = 1 << 7
+        ws = access.wsmake(mem=range(10000), rand=random, size=j)
+        mms.alist = access.makealist(range(10000), ws, random, 0.95, 100000)
+        ratios.append((j, simulate.simulate(mms, mms.alist)))
+        print i.__name__ + ": " + str(j)
+    d.append(Gnuplot.Data(ratios, title=i.__name__, inline=1))
+
+g = Gnuplot.Gnuplot()
+#g('set data style lines')
+#g('set style data lines')
+g('set style data linespoints')
+g('set yrange [0:]')
+g('set terminal epslatex monochrome')
+g('set output "diagram1.eps"')
+g('set xlabel "working set size in pages"')
+g('set ylabel "ratio of page faults to accesses"')
+g.plot(*d)
+
+g('set terminal png')
+g('set output "diagram1.png"')
+g.plot(*d)
+
+#print "\nNow move diagram1.* to ../"
